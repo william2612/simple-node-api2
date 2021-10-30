2c8154b76988850e1b0828f9bec531c98e905ea3
William <william8fir@hotmail.com>
2021-10-30 11:50:52 -0300

    initial commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.port || 8000;
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello There!');
+    res.send('Hello World!');
 });
 
 app.listen(port, () => { console.log('Our App Is Up And Running!'); });
